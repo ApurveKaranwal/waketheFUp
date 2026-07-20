fb542eb363189d1ad8f48362ca32ee8b2c1c8391
ApurveKaranwal <apurvekaranwal282@gmail.com>
2026-07-20 22:58:26 +0530

    completed manual pinging api

--- backend/controllers/manualPingJob.ts
+++ backend/controllers/manualPingJob.ts
@@ -5,6 +5,7 @@ export default async function manualPingJob(c: Context) {
   try {
     const projectId = c.req.param('id');
     const userId = "cmrf8wtxs0000x3xysq2iro6x";
+    const checkedAt = new Date();
 
     const project = await prisma.project.findFirst({
       where: {
@@ -22,14 +23,50 @@ export default async function manualPingJob(c: Context) {
       );
     }
 
-    const start = Date.now();
+    const start = performance.now();
     const response = await fetch(project.url);
-    const end = Date.now();
-    const responseTime = end - start;
+    const responseTime = Math.round(performance.now() - start);
 //we can also use performance.now() for much better accuracy
-    return c.json({
-      status: response.status,
-      responseTime,
+
+    await prisma.project.update({
+      where: {
+        id: project.id,
+      },
+      data: {
+        lastStatus: response.status,
+        lastCheckedAt: checkedAt,
+        failureCount: 0,
+      },
     });
+
+    await prisma.pingHistory.create({
+      data: {
+        status: response.status,
+        responseTime,
+        projectId: project.id,
+      },
+    });
+
+    return c.json({
+      success: true,
+      message: "Project checked Successfully",
+      data: {
+        status: response.status,
+        responseTime,
+        checkedAt,
+      },
+    },
+      200
+    );
+  }
+  catch (err) {
+    console.error(err);
+
+    return c.json({
+      success: false,
+      message: "Internal Server Error",
+    },
+      500
+    );
   }
 }
